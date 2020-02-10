b107d79796445dc869dcb4a7fde650f69777e61a
norihirosunada <norisuna1201@gmail.com>
2020-02-10 17:34:18 +0000

    012, done

---
+++ models.rb
@@ -0,0 +1,15 @@
+require 'bundler/setup'
+Bundler.require
+
+if development?
+    ActiveRecord::Base.establish_connection("sqlite3:db/development.db")
+end
+
+class User < ActiveRecord::Base
+    has_secure_password
+    validates :name,
+        presence: true,
+        format: {with: /\A\w+\z/}
+    validates :password,
+        length: {in: 5..10}
+end
